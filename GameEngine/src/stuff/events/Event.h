87ba581d142ac703ac19b6cc1012e875efbe8465
chennj <chennj_cn@aliyun.com>
2021-03-29 15:34:12 +0800

    Game Engine: OpenGL Mathematics (GLM)

--- GameEngine/src/stuff/events/Event.h
+++ GameEngine/src/stuff/events/Event.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../Core.h"
+#include "stuff\Core.h"
 
 namespace SOMEENGINE
 {
--- premake5.lua
+++ premake5.lua
@@ -16,6 +16,7 @@ IncludeDir = {}
 IncludeDir["GLFW"]	= "GameEngine/vendor/glfw/include"
 IncludeDir["GLAD"]	= "GameEngine/vendor/glad/include"
 IncludeDir["ImGui"] = "GameEngine/vendor/imgui"
+IncludeDir["glm"]	= "GameEngine/vendor/glm"
 
 include "GameEngine/vendor/glfw"
 include "GameEngine/vendor/glad"
@@ -45,7 +46,8 @@ project "GameEngine"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.GLAD}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 	links
@@ -104,13 +106,16 @@ project "Sandbox"
 	files
 	{
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
 	}
 
 	includedirs
 	{
 		"GameEngine/vendor/spdlog/include",
-		"GameEngine/src"
+		"GameEngine/src",
+		"%{IncludeDir.glm}"
 	}
 
 	links
